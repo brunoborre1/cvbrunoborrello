a86e424652f28aa1574c3fc486d246f61cdd17bb
brunoborre1 <brunoborrello@gmail.com>
2021-08-30 01:09:25 -0300

    Envio proyecto de curriculum

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Curriculum Vitae - Bruno</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Bruno Fabian Borrello</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/brunoborrelloCV.jpeg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Sobre Mi</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experiencia</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Educacion</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Habilidades</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Intereses</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Titulos</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Bruno Fabian
+                        <span class="text-primary">Borrello</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Berutti 765 · Las Heras, CP 5539 · (261) 256-8208 ·
+                        <a href="mailto:name@email.com">brunoborrello@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">Tengo amplia experiencia en el sector de atencion al publico como asi tambien, en el sector infórmatico. Me considero una persona proactiva con metas de superacion en lo laboral </p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/bruno-borrello-92b967b4/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/brunoborre1"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://www.facebook.com/bruno.borrello.7/"><i class="fab fa-twitter"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experiencia Laboral</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Desarrollador Web Junior</h3>
+                            <div class="subheading mb-3">Cup Studio</div>
+                            <p>Me desempeño como desarrollardor junior</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">March 2021 - Actualidad</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Adminsitrativo</h3>
+                            <div class="subheading mb-3">Clincia Araucaria</div>
+                            <p>Cumpli tareas en el sector de administracion, las tareas principales consistian en liquidacion de sueldos, realizacion de pagos a proveedores, control de personal</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Junio 2018 - Actualidad</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Crupier Junior</h3>
+                            <div class="subheading mb-3">Casino Enjoy Mendoza</div>
+                            <p>Me desempeñe como crupier durante 5 años. Realice academias de Poker en los estilos de Texas, Draw y Caribean, academia de ruleta y Black Jack</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Agosto 2008 - Septiembre 2013</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Insituto Superior Manuel Belgrano</h3>
+                            <div class="subheading mb-3">Analista en Sistemas</div>
+                            <div>Cursado el total de la carrera. Actualmente adeudo 7 materias</div>                           
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Marzo 2000 - Diciembre 2004</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Instituto San Miguel - 93 Ps</h3>
+                            <div class="subheading mb-3">“Economía y Gestión de las Organizaciones” con orientación en gestión contable, impositiva laboral y provisional </div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Febrero 2014 - Diciembre 2016</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Habilidades</h2>
+                    <div class="subheading mb-3">Lenguajes de programacion y Herramientas</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                        <!--
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-angular"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-sass"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-less"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
+                        -->
+                    </ul>
+                    <div class="subheading mb-3">Flujo de trabajo</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Responsive Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Intereses</h2>
+                    <p>Además de ser desarrollador web, disfruto la mayor parte del tiempo al aire libre. Disfruto mucho de realizar deportes como futbol, natacion y balon cesto.
+                       Disfruto del ciclismo de montaña durante los meses mas calurosos.</p>
+                    <p class="mb-0">Cuando me toca en casa disfruto mucho viendo series o peliculas. Tambien me intereso por ver tutoriales de programacion para aumentar mis habilidades</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <section class="resume-section" id="awards">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Certificaciones</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Reparacion y mantenimiento de pcs.
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Laravel 8 - Cerificado por Udemy
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            <!-- <sup>st</sup> -->
+                            Desarrollador Web - Certifcado por Platzi
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
